b07dcf518dc11ad64127aa67990b27caf72dd9bc
Heather Downing <quorralyne@gmail.com>
2019-10-02 08:41:22 -0500

    Added okta for auth

--- ListAPI/ListAPI/Controllers/ListItemsController.cs
+++ ListAPI/ListAPI/Controllers/ListItemsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,33 +8,74 @@ using System.Web.Http;
 
 namespace ListAPI.Controllers
 {
+    [Authorize]
     public class ListItemsController : ApiController
     {
+        private static List<CustomListItem> _listItems { get; set; } = new List<CustomListItem>();
+
         // GET: api/ListItems
-        public IEnumerable<string> Get()
+        public IEnumerable<CustomListItem> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _listItems;
         }
 
         // GET: api/ListItems/5
-        public string Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return "value";
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
 
         // POST: api/ListItems
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody]CustomListItem model)
         {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var maxId = 0;
+            if (_listItems.Count > 0)
+            {
+                maxId = _listItems.Max(x => x.Id);
+            }
+            model.Id = maxId + 1;
+            _listItems.Add(model);
+
+            return Request.CreateResponse(HttpStatusCode.Created, model);
         }
 
         // PUT: api/ListItems/5
-        public void Put(int id, [FromBody]string value)
+        public HttpResponseMessage Put(int id, [FromBody]CustomListItem model)
         {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                // Update *all* of the item's properties
+                item.Text = model.Text;
+
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
 
         // DELETE: api/ListItems/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _listItems.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
     }
 }
